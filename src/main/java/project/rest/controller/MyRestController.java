fe7d2837cc9cae95e5625ee7e708d6cef329bfa9
Eilish Ni Thuama <Eilish Ni Thuama>
2017-03-28 09:23:49 +0100

    Updated Pom

--- src/main/java/project/rest/controller/MyRestController.java
+++ src/main/java/project/rest/controller/MyRestController.java
@@ -19,11 +19,8 @@ import project.rest.service.FallService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@Controller
 @RestController
 public class MyRestController {
 	
